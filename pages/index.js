2091d9b8be0ce395bfceda1ab25d0733366295a2
iamSenna <127565614+iamSenna@users.noreply.github.com>
2024-08-31 15:35:34 +0000

    modifica a frase

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home() {
-    return <h1>fui esculpido a machado mas eu ainda tenho coração</h1>
+    return <h1>underground tem muito mais caneta que mainstream. isso é um fato.</h1>
 }
 
 export default Home;
