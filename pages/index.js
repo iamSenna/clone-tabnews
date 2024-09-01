dbf6872965dcbbdbbbb2c7302d34202c80198378
iamSenna <127565614+iamSenna@users.noreply.github.com>
2024-09-01 16:47:27 +0000

    adiciona scripts `lint:check` e `lint:fix`

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,7 @@
 function Home() {
-    return <h1>underground tem muito mais caneta que mainstream. isso é um fato.</h1>
+  return (
+    <h1>underground tem muito mais caneta que mainstream. isso é um fato.</h1>
+  );
 }
 
 export default Home;
